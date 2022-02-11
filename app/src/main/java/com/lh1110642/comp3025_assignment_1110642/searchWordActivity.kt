65f1d53003793cb42b4c917a199a9bb895af8fb5
awais <nisarawais246@gmail.com>
2022-02-10 20:42:27 -0500

    scaffolded a new activity

---
+++ app/src/main/java/com/lh1110642/comp3025_assignment_1110642/searchWordActivity.kt
@@ -0,0 +1,11 @@
+package com.lh1110642.comp3025_assignment_1110642
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class searchWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_word)
+    }
+}
