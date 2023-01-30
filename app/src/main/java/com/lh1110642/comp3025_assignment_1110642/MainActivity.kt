bbf42475833363f079037f10d9e604db47a5201b
aanisar <55286102+aanisar@users.noreply.github.com>
2023-01-29 22:55:38 -0500

    American Sign Language Dictionary App

--- app/src/main/java/com/lh1110642/comp3025_assignment_1110642/MainActivity.kt
+++ app/src/main/java/com/lh1110642/comp3025_assignment_1110642/MainActivity.kt
@@ -9,12 +9,13 @@ import android.os.Looper
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.lh1110642.comp3025_assignment_1110642.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-//    private lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,20 +28,20 @@ class MainActivity : AppCompatActivity() {
         }, 1000)
 
 
-
+//
 //        auth = FirebaseAuth.getInstance()
-
+//
 //        // navigate to the sign up page
-//        binding.signupButton.setOnClickListener {
+//        binding.signupButton?.setOnClickListener {
 //            startActivity(Intent(this, SignUpActivity::class.java))
 //        }
 //
 //        // navigate to the log in page
-//        binding.loginButton.setOnClickListener {
+//        binding.loginButton?.setOnClickListener {
 //            startActivity(Intent(this, SignInActivity::class.java))
 //        }
 //    }
-
+//
 //    public override fun onStart() {
 //        super.onStart()
 //        // Check if user is signed in (non-null) and update UI accordingly.
@@ -48,7 +49,7 @@ class MainActivity : AppCompatActivity() {
 //        if (currentUser != null) {
 //            reload();
 //        }
-    }
+//    }
 //    private fun reload() {
 //            startActivity(Intent(this, InsideMainActivity::class.java))
 //        }
@@ -58,5 +59,5 @@ class MainActivity : AppCompatActivity() {
 //        a.addCategory(Intent.CATEGORY_HOME)
 //        a.flags = Intent.FLAG_ACTIVITY_NEW_TASK
 //        startActivity(a)
-//    }
+    }
 }
